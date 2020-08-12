07341fc5f947c75ee62558ce1f154e93fccf9ebe
Elder Ospino <elderospino@gmail.com>
2020-08-12 00:52:58 -0500

    Se agrega pruebas unitarias del servicio y del API REST, implementando JUnit + Mockito

--- src/test/kotlin/com/apirestKo/apirestKo/ApirestKoApplicationTests.kt
+++ src/test/kotlin/com/apirestKo/apirestKo/ApirestKoApplicationTests.kt
@@ -1,13 +1,59 @@
 package com.apirestKo.apirestKo
 
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
+import org.mockito.Mockito
 
+import com.apirestKo.apirestKo.dao.IPersonaRepository
+import com.apirestKo.apirestKo.model.Persona
+import com.apirestKo.apirestKo.services.PersonaServicesImpl
+import org.junit.*
+
+import org.junit.runner.RunWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.springframework.boot.test.context.SpringBootTest
+import org.mockito.junit.MockitoJUnitRunner;
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+@RunWith(MockitoJUnitRunner::class)
 @SpringBootTest
 class ApirestKoApplicationTests {
 
-	@Test
-	fun contextLoads() {
+	@Mock
+	val personaRepository: IPersonaRepository? = null
+
+	@InjectMocks
+	val persosnaServices: PersonaServicesImpl? = null
+
+
+	@Before
+	fun before() {
+		println("Before");
 	}
 
+	@After
+	fun after() {
+		println("After");
+	}
+
+
+	@Test
+	fun contextLoads() {
+		println("inicio test................")
+
+		val formatter = DateTimeFormatter.ofPattern("dd-MM-yyy")
+		val persona1 = Persona("126", "Elder", "Ospino", LocalDate.parse("05-08-1981", formatter))
+
+		var list:List<Persona> = listOf(persona1)
+
+
+
+
+		Mockito.`when`(personaRepository!!.findAll()).thenReturn(list)
+		Assert.assertNotNull(personaRepository!!.findAll())
+
+		Assert.assertEquals(1, personaRepository!!.findAll().filter { t -> t.identificacion == "126" }.count())
+
+
+	}
 }
--- src/test/kotlin/com/apirestKo/apirestKo/ControllerTest/PersonaRestControllerTest.kt
+++ src/test/kotlin/com/apirestKo/apirestKo/ControllerTest/PersonaRestControllerTest.kt
@@ -1,17 +1,76 @@
 package com.apirestKo.apirestKo.ControllerTest
 
-import com.apirestKo.apirestKo.controller.PersonaRestController
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.boot.test.context.SpringBootTest
+import com.apirestKo.apirestKo.model.Persona
+import com.apirestKo.apirestKo.utils.Constans
 
-@SpringBootTest
-//@Run
-//@WebMvcTest(controllers = PersonaRestController::class)
+import org.junit.*
+
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.ResponseEntity
+import org.springframework.test.context.junit4.SpringRunner
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+class CatsList : MutableList<Persona> by ArrayList()
+
+@RunWith(SpringRunner::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PersonaRestControllerTests {
 
+    @Autowired
+    lateinit var testRestTemplate: TestRestTemplate
+
+
+    @Before
+    fun before() {
+        println("Before");
+    }
+
+    @After
+    fun after() {
+        println("After");
+    }
+
+
     @Test
-    fun contextLoads() {
+     fun list() {
+        println("*************** API REST Listar Personas ****************")
+
+        val result = testRestTemplate.getForEntity(Constans.URL_BASE_PERSONAS, CatsList::class.java)
+
+
+        Assert.assertEquals(200, result.statusCode.value())
+
+
+        //result.body!!.forEach { t ->  println("Identificacion: " + t.identificacion)}
+
+
+        Assert.assertEquals(1, result.body!!.filter { t -> t.identificacion == "12603" }.count())
+
+
+
+    }
+
+    @Test
+    fun insert() {
+        println("*************** API REST Insert Personas ****************")
+
+        val formatter = DateTimeFormatter.ofPattern("dd-MM-yyy")
+        val persona1 = Persona("454545", "Julano", "Detal", LocalDate.parse("05-08-1981", formatter))
+
+
+        val result = testRestTemplate.postForEntity(Constans.URL_BASE_PERSONAS, persona1, Any::class.java)
+
+
+        Assert.assertEquals(201, result.statusCode.value())
+
+        val result2 = testRestTemplate.getForEntity(Constans.URL_BASE_PERSONAS, CatsList::class.java)
+        Assert.assertEquals(1, result2.body!!.filter { t -> t.identificacion == "454545" }.count())
+
+
     }
 
 }
